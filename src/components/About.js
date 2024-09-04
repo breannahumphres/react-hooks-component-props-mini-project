c3271b48e137a83cbae06f5effee9fdfd44b5be2
Bre Humphres <breannahumphres@gmail.com>
2024-09-04 16:54:55 -0700

    all tests passing

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+function About({image, about}) {
+    return (
+        <aside>
+            <img alt = "blog logo" src = {image ? image: "https://via.placeholder.com/215"} />
+            <p>{about}</p>
+        </aside>
+    )
+}
+export default About;
